0c4e35eaaeb5a56665327d4f80f1b2c87718918a
Anand S <anandsatheeshk@gmail.com>
2023-04-05 12:52:49 +0530

    new quote button displays new quote

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <div id="quote-box">
-        <p id="text">quote text here</p>
-        <p id="author">quote author</p>
-        <a href="#" id="tweet-quote">Tweet</a>
-        <button id="new-quote">New quote</button>
-      </div>
-    </div>
+    <QuoteMachine />
   );
+}
+
+class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteList: [
+        {
+          quote: 'quote',
+          author: 'author'
+        },
+        {
+          quote: 'Dream big and dare to fail.',
+          author: '- Norman Vaughan'
+        }, 
+        {
+          quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.',
+          author: '- Joshua J. Marine'
+        },
+        {
+          quote:'Change your thoughts and you change your world.',
+          author: '- Norman Vincent Peale'
+        }
+      ],
+      quoteNumber: Math.floor(Math.random() * 4)
+    }
+  }
+
+  // quoteNumber = Math.floor(Math.random() * quoteList.length);
+
+  newQuote = () => {
+    this.setState({
+      quoteNumber: Math.floor(Math.random() * 4)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="quote-box">
+          <p id="text">{this.state.quoteList[this.state.quoteNumber].quote}</p>
+          <p id="author">{this.state.quoteList[this.state.quoteNumber].author}</p>
+          <a href="#" id="tweet-quote">Tweet</a>
+          <button id="new-quote" onClick={this.newQuote}>New quote</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
